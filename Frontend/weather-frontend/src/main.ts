7cc09b02d5e0fcc0cf7e3dc3ca615593680cd62d
Joshua <joginoparez@gmail.com>
2026-09-23 14:52:10 +0200

    [DEV] ERROR manage & Location name FIX - Now if you enter a city that doesn't exist, the frontend displays the ERROR sent back by the API - The name of the city is writen next to "Weather in ..." and even if you don't write correctly the city

--- Frontend/weather-frontend/src/main.ts
+++ Frontend/weather-frontend/src/main.ts
@@ -10,9 +10,14 @@ fetchWeatherBtn.addEventListener('click', async () => {
   const response = await fetch(`http://127.0.0.1:8000/weather/${city}`)
   const data = await response.json()
 
+  if (!response.ok) {
+    resultsDiv.innerHTML = `<p>Error: ${data.detail}</p>`
+    return
+  }
+
 // le parametre current obligatoire pr accéder aux données sinon ça ecrit undefined, dans l'url dans le backend on retrouve ce param current !!
   resultsDiv.innerHTML = `
-    <h2>Weather in ${data.current.city}</h2>
+    <h2>Weather in ${data.location}</h2>
     <p>Temperature: ${data.current.temperature_2m}°C</p>
     <p>Humidity: ${data.current.relative_humidity_2m}%</p>
     <p>Wind Speed: ${data.current.wind_speed_10m} m/s</p>
