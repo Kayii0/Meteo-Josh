b7a9786aa18707259da06e22713fd0c21ca9213d
Joshua <joginoparez@gmail.com>
2026-09-23 12:20:32 +0200

    [DEV] Now Frontend and the backend is connected - index.html : I create a form to enter a city and send it to recover the name of the city and after the TS ask the API with this name. - main.ts : he ask the backend API for value and display them.

--- Frontend/weather-frontend/src/main.ts
+++ Frontend/weather-frontend/src/main.ts
@@ -1,60 +1,21 @@
 import './style.css'
-import heroImg from './assets/hero.png'
-import typescriptLogo from './assets/typescript.svg'
-import viteLogo from './assets/vite.svg'
-import { setupCounter } from './counter.ts'
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-<section id="center">
-  <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${typescriptLogo}" class="framework" alt="TypeScript logo"/>
-    <img src="${viteLogo}" class="vite" alt="Vite logo" />
-  </div>
-  <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/main.ts</code> and save to test <code>HMR</code></p>
-  </div>
-  <button id="counter" type="button" class="counter"></button>
-</section>
+const cityInput = document.querySelector<HTMLInputElement>('#cityInput')!
+const fetchWeatherBtn = document.querySelector<HTMLButtonElement>('#fetchWeatherBtn')!
+const resultsDiv = document.querySelector<HTMLDivElement>('#results')!
 
-<div class="ticks"></div>
+fetchWeatherBtn.addEventListener('click', async () => {
+  const city = cityInput.value
 
-<section id="next-steps">
-  <div id="docs">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#documentation-icon"></use></svg>
-    <h2>Documentation</h2>
-    <p>Your questions, answered</p>
-    <ul>
-      <li>
-        <a href="https://vite.dev/" target="_blank">
-          <img class="logo" src="${viteLogo}" alt="" />
-          Explore Vite
-        </a>
-      </li>
-      <li>
-        <a href="https://www.typescriptlang.org" target="_blank">
-          <img class="button-icon" src="${typescriptLogo}" alt="">
-          Learn more
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div id="social">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#social-icon"></use></svg>
-    <h2>Connect with us</h2>
-    <p>Join the Vite community</p>
-    <ul>
-      <li><a href="https://github.com/vitejs/vite" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
-      <li><a href="https://chat.vite.dev/" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#discord-icon"></use></svg>Discord</a></li>
-      <li><a href="https://x.com/vite_js" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#x-icon"></use></svg>X.com</a></li>
-      <li><a href="https://bsky.app/profile/vite.dev" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#bluesky-icon"></use></svg>Bluesky</a></li>
-    </ul>
-  </div>
-</section>
+  const response = await fetch(`http://127.0.0.1:8000/weather/${city}`)
+  const data = await response.json()
 
-<div class="ticks"></div>
-<section id="spacer"></section>
-`
-
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// le parametre current obligatoire pr accéder aux données sinon ça ecrit undefined, dans l'url dans le backend on retrouve ce param current !!
+  resultsDiv.innerHTML = `
+    <h2>Weather in ${data.current.city}</h2>
+    <p>Temperature: ${data.current.temperature_2m}°C</p>
+    <p>Humidity: ${data.current.relative_humidity_2m}%</p>
+    <p>Wind Speed: ${data.current.wind_speed_10m} m/s</p>
+    <p>UV Index: ${data.current.uv_index}</p>
+  `
+})
